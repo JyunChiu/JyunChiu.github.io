a3ab7a85320566362abc2019cf287063af643ffc
jin <jin_chiu@wistron.com>
2020-08-28 14:44:22 +0800

    Updates

---
+++ precache-manifest.1a0f21ee719019667f0d2b4020c228e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaccf1f24425379c719ea3aa7e5e77e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ed9d1a02b4fe01230f8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f2d4a8d375edba7acb28",
+    "url": "/static/js/2.632fa5e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.632fa5e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ed9d1a02b4fe01230f8",
+    "url": "/static/js/main.7022bbb4.chunk.js"
+  },
+  {
+    "revision": "f51a9a4cc6b1c7977f9b",
+    "url": "/static/js/runtime-main.49ff3910.js"
+  },
+  {
+    "revision": "c38ac810afc9494d98bfdf27b8ce2d6d",
+    "url": "/static/media/error.c38ac810.svg"
+  },
+  {
+    "revision": "0a1ca4e306ff1dd066906c2419b88552",
+    "url": "/static/media/time.0a1ca4e3.svg"
+  }
+]);
