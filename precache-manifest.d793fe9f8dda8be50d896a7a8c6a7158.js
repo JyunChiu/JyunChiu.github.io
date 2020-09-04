3593ff88109d9b5fb50751e553e579ff0fdf1f9a
jin <jin_chiu@wistron.com>
2020-09-04 17:59:38 +0800

    Updates

---
+++ precache-manifest.d793fe9f8dda8be50d896a7a8c6a7158.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1240ae6c9a7ecefc1c894fbb6b0e5f03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc2d599ece62becf262e",
+    "url": "/static/css/main.f1155f3e.chunk.css"
+  },
+  {
+    "revision": "97988deaef0f9490ce86",
+    "url": "/static/js/3.21e2b172.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.21e2b172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc046eef715a1434020",
+    "url": "/static/js/About.3855b4dc.chunk.js"
+  },
+  {
+    "revision": "dc2d599ece62becf262e",
+    "url": "/static/js/main.a29cc559.chunk.js"
+  },
+  {
+    "revision": "c3ed172ce80e0f837ad8",
+    "url": "/static/js/runtime-main.72a07244.js"
+  }
+]);
